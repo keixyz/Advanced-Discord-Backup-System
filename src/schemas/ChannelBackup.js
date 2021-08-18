d5bba9bf3c90b82147f753e5542cbbe9205aa9cf
keixyz <72768341+keixyz@users.noreply.github.com>
2021-08-18 16:40:27 +0300

    Delete ChannelBackup.js

--- src/schemas/ChannelBackup.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const schema = new Schema({
-    guildID: String,
-    roleID: String,
-    channelOverwrites: Array
-});
-
-module.exports = model("channelbackupfenice", schema);
